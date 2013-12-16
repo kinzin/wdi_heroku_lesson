5f879e57bbd4cfdfc037d19240c3cab24d37431d
kinzin <yniznik@gmail.com>
2013-12-16 11:42:09 -0500

    music app launch 2

--- app.rb
+++ app.rb
@@ -4,14 +4,14 @@ Bundler.require(:default)
 require_relative 'modules'
 
 get '/' do 
-erb :index
+	erb :index
 end
 
 get '/artist' do 
-input = params[:artist]
-url = UrlFinder.create_artist_url(input)
-response = HTTParty.get url
-details = JSON response.body
-@list_of_things = details["cdstubs"]
-erb :artist
+	input = params[:artist]
+	url = UrlFinder.create_artist_url(input)
+	response = HTTParty.get url
+	details = JSON response.body
+	@list_of_things = details["cdstubs"]
+	erb :artist
 end
